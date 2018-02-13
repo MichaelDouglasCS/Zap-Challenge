ff5822900f3c15f64717054da32447fbc2193b47
Michael Douglas <michael.dcsouza@gmail.com>
2018-02-13 12:11:16 -0200

    - Remove unnecessary classes,  - Create PersistenceService class

---
+++ Zap-Challenge/Sources/Persistence/PersistenceService.swift
@@ -0,0 +1,20 @@
+//
+//  PersistenceService.swift
+//  Zap-Challenge
+//
+//  Created by Michael Douglas on 13/02/18.
+//  Copyright © 2018 Michael Douglas. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+//**********************************************************************************************************
+//
+// MARK: - Class -
+//
+//**********************************************************************************************************
+
+public class PersistenceService: NSObject {
+  
+}
