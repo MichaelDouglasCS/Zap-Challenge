443a6918ff9394a639fe797312450b8122a431c9
Michael Douglas <michael.dcsouza@gmail.com>
2018-06-24 23:10:44 -0300

    - Small adjusts

--- Zap-Challenge/Sources/Features/GameDetails/Model/GameDetailsProvider.swift
+++ Zap-Challenge/Sources/Features/GameDetails/Model/GameDetailsProvider.swift
@@ -15,25 +15,25 @@ import Foundation
 //**********************************************************************************************************
 
 public class GameDetailsProvider: NSObject {
-
-  //*************************************************
-  // MARK: - Exposed Methods
-  //*************************************************
-  
-  public func addFavoriteGame(_ gameRank: GameRank) {
-    _ = gameRank.toNSManagedObject()
-    PersistenceService.shared.saveContext()
-  }
-
-  public func removeFavoriteGame(_ gameRank: GameRank) {
-    do {
-      if let games = try PersistenceService.shared.context.fetch(GameRankMO.fetchRequest()) as? [GameRankMO] {
-        games.forEach({
-          if $0.game?.id == Int32(gameRank.game?.id ?? 0) {
-            PersistenceService.shared.context.delete($0)
-          }
-        })
-      }
-    } catch { }
-  }
+    
+    //*************************************************
+    // MARK: - Exposed Methods
+    //*************************************************
+    
+    public func addFavoriteGame(_ gameRank: GameRank) {
+        _ = gameRank.toNSManagedObject()
+        PersistenceService.shared.saveContext()
+    }
+    
+    public func removeFavoriteGame(_ gameRank: GameRank) {
+        do {
+            if let games = try PersistenceService.shared.context.fetch(GameRankMO.fetchRequest()) as? [GameRankMO] {
+                games.forEach({
+                    if $0.game?.id == Int32(gameRank.game?.id ?? 0) {
+                        PersistenceService.shared.context.delete($0)
+                    }
+                })
+            }
+        } catch { }
+    }
 }
